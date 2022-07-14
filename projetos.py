9ccb457ae0340bfdb2e48224e521cdc2887dad74
adrianofr500 <108909830+adrianofr500@users.noreply.github.com>
2022-07-14 19:43:58 -0300

    menu
    tela principal projeto

--- projetos.py
+++ projetos.py
@@ -1,1 +1,32 @@
-print('ola mundo')
+def programa():
+    opcao = menu()
+    if opcao.lower() == 'a':
+        print('cadastrada produto\n')
+    else:
+        print('opção invalida\n')    
+    
+     
+   
+  
+
+def menu():
+    print('=-='*14)
+    print('     SISTEMA DE GESTÃO DE PRODUTOS')
+    print('=-='*14)
+    print('\n')
+    print('     <!> Seja bem vindo(a)!\n')
+    print('     >>> Operações desponíveis:\n')
+    print('     <A> Cadastrar produto')
+    print('     <B> Consulta simples')
+    print('     <C> Consulta geral')
+    print('     <D> Deletar produto')
+    print('     <E> informações do produto')
+    print('     <F> Informação do sistema')
+    print('     <G> Sair do sitema\n')
+    print('     <?> o que você deseja fazer?')
+    entrada = input(' digite a opção')
+    return entrada
+
+while True:
+    programa()    
+    
